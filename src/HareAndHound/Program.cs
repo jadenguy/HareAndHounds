88da46545a0653d4c66be5866679e1ddafe78bf6
Alfredo Inostroza <jadenguy@gmail.com>
2020-08-01 13:45:20 -0400

    WriteHost added since I use that a lot

--- src/HareAndHound/Program.cs
+++ src/HareAndHound/Program.cs
@@ -8,8 +8,25 @@ namespace HareAndHound
         static void Main(string[] args)
         {
             var board = new Board();
-            Console.WriteLine(board.Print());
+            board.Print().WriteHost();
             var next = board.NextStates().ToArray();
+            next.Count().WriteHost();
+            next.Distinct().Count().WriteHost();
+        }
+    }
+    static class WriterExtension
+    {
+        public static void WriteHost(this object message)
+        {
+            System.Diagnostics.Debug.WriteLine(message);
+            System.Console.WriteLine(message);
+        }
+        public static void WriteHost(this object message, object title)
+        {
+            var mString = message.ToString();
+            var tString = title.ToString();
+            var mOut = tString + ": " + mString;
+            mOut.WriteHost();
         }
     }
 }
